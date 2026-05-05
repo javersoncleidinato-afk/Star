5eb959aa2bd81bd3dd38d4a34a68b45175696c9b
javersoncleidinato-afk <javersoncleidinato@gmail.com>
2026-05-05 11:51:43 -0300

    Add apology messages in Yay.py

---
+++ Yay.py
@@ -0,0 +1,6 @@
+print("sorry for waiting stars")
+print("very sorry")
+print("but I really want it")
+print("I need 30 stars")
+print("I know it's too much")
+print(" but if you can help me, thanks:)")
